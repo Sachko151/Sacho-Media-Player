03399e867538d2621173a1db48fec58e632d7825
Sachko151 <sachko151@abv.bg>
2023-02-05 20:39:03 +0200

    Added the ability to load a file

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,2 +1,3 @@
+clear
 gcc `pkg-config --cflags gtk+-3.0` main.c -o output.out `pkg-config --libs gtk+-3.0` -rdynamic -lvlc
 ./output.out
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <gtk-3.0/gtk/gtkx.h>
 #include <stdlib.h>
 #include <vlc/vlc.h>
+#include <stddef.h>
 //Declare the global variables
 GtkWidget *window;
 GtkWidget *fixed;
@@ -11,6 +12,7 @@ GtkWidget *btnStop;
 GtkWidget *btnPause;
 GtkWidget *btnPlay;
 GtkWidget *btnLoad;
+GtkWidget *btnFileChooser;
 GtkWidget *lblProgress;
 GtkBuilder *builder;
 libvlc_instance_t *inst;
@@ -18,6 +20,7 @@ libvlc_media_player_t *mp;
 libvlc_media_t *file;
 void init_the_gui(int argc, char **argv);
 void init_the_media_player(libvlc_media_t *file);
+void logTheProgress(libvlc_media_player_t *mp);
 int main(int argc, char **argv){
     init_the_gui(argc, argv);
     return 0;
@@ -28,6 +31,7 @@ void init_the_gui(int argc, char **argv){
     builder = gtk_builder_new_from_file("gui.glade");
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    
     gtk_builder_connect_signals(builder, NULL);
     fixed = GTK_WIDGET(gtk_builder_get_object(builder, "fixed"));
     lbl_media_name = GTK_WIDGET(gtk_builder_get_object(builder, "lblMediaName"));
@@ -35,9 +39,15 @@ void init_the_gui(int argc, char **argv){
     btnPause = GTK_WIDGET(gtk_builder_get_object(builder, "btnPause"));
     btnPlay = GTK_WIDGET(gtk_builder_get_object(builder, "btnPlay"));
     btnLoad = GTK_WIDGET(gtk_builder_get_object(builder, "btnPLoad"));
+    btnFileChooser = gtk_file_chooser_button_new(("Select a file"),
+                                        GTK_FILE_CHOOSER_ACTION_OPEN);
     lblProgress = GTK_WIDGET(gtk_builder_get_object(builder, "lblProgress"));
     gtk_widget_show(window);
     gtk_main();
+}
+void logTheProgress(libvlc_media_player_t *mp){
+    int64_t time_in_seconds = (int64_t)libvlc_media_player_get_time(mp) / 1000;
+    printf("%ld\n", time_in_seconds);
 }
 void init_the_media_player(libvlc_media_t *file){
     inst = libvlc_new(0, NULL);
@@ -61,6 +71,8 @@ void on_btnPause_clicked(GtkButton *button){
         printf("%s", "not loaded!");
         return;
     }
+    printf("%s", "Pausing music\n");
+    logTheProgress(mp);
     libvlc_media_player_pause(mp);
 }
 void on_btnStop_clicked(GtkButton *button){
@@ -73,7 +85,16 @@ void on_btnStop_clicked(GtkButton *button){
 }
 void on_btnLoad_clicked(GtkButton *button){
     printf("%s", "Loading File\n");
-    file = libvlc_media_new_path(inst, "/home/sachko/Music/Background Music/Cipher.mp3");
+    file = libvlc_media_new_path(inst, "/home/sachko/Music/Background Music/Melancholy.mp3");
     init_the_media_player(file);
     gtk_label_set_text(GTK_LABEL(lbl_media_name), (gchar*) "Cipher.mp3");
+}
+void on_btnFileChooser_file_set(GtkFileChooserButton *button, gpointer user_data){
+    printf("%s\n", gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(button)));
+    file = libvlc_media_new_path(inst, gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(button)));
+    init_the_media_player(file);
+    printf("%s", "set_file\n");
+}
+void on_btnFileChooser_selection_changed(GtkFileChooserButton *button){
+    printf("%s", "selection_changed\n");
 }
